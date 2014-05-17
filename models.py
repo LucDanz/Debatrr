45d16f49a13a0480549111fdfb5a35753d1748eb
Eric <ericdanziger@gmail.com>
2014-05-16 23:12:50 -0400

    working on models

--- models.py
+++ models.py
@@ -35,7 +35,7 @@ class Argument(db.Model):
 
 class Debate(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(String(100))
+	name = db.Column(db.String(100))
         proTeamID = db.Column(db.Integer)
         conTeamID = db.Column(db.Integer)
         date = db.Column(db.DateTime)
@@ -47,6 +47,27 @@ class Debate(db.Model):
 		self.date = date
 
 class Team(db.Model):
-	
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100))
+	memberList = db.Column(db.Text)
+
+	def __init__(self, name, memlist):
+		self.name = name
+		self.memberList = memlist
+
+class Fact(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	argID = db.Column(db.Integer)
+	text = db.Column(db.Text)
+	link = db.Column(db.String(100))
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(30))
+	email = db.Column(db.String(50))
+	pass = db.Column(db.String(200))
+	salt = db.Column(db.String(100))
+
+
 
 
