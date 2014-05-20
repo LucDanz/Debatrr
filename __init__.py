778846605f11debbcf9f2c5e134014274c7e2411
Eric <ericdanziger@gmail.com>
2014-05-19 20:30:35 -0700

    flask mega tutorial

---
+++ __init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+from app import views
+
+
+@app.route('/')
+@app.route('/index')
+def arg():
+    return "hello args"
