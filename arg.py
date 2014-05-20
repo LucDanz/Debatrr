149ae5bdcaf74a3b4d5bd48ec2cd233fb79200f4
Eric <ericdanziger@gmail.com>
2014-05-19 20:43:08 -0700

    fixing main py file

--- arg.py
+++ arg.py
@@ -3,7 +3,9 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import views
+db = SQLAlchemy(app)
+
+from app import views, models
 
 
 @app.route('/')
