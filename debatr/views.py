ba4ef2dd8a4a276139573b338c3c5e491034e677
Eric <ericdanziger@gmail.com>
2014-05-21 13:00:12 -0700

    debugging

--- debatr/views.py
+++ debatr/views.py
@@ -5,7 +5,7 @@ import bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask import render_template
-from debapp import app
+from debatr import app
 
 @app.route('/')
 @app.route('/index')
