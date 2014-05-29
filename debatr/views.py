12989e78a0e642def54ef31588fe173204cec5c3
Eric Danz <ericdanziger@gmail.com>
2014-05-28 20:36:36 -0400

    import models in views

--- debatr/views.py
+++ debatr/views.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask import render_template
 from debatr import app
+from debatr import models
 
 @app.route('/')
 @app.route('/index')
@@ -18,12 +19,12 @@ def login():
 
 @app.route('/debate')
 def debate():
-	debates = Debate.query.limit(1).all()[0]
-	res = Resolution.query.filter_by(id=debates.resID).all()[0]
-	proTeam = Team.query.filter_by(id=debates.proTeamID).all()[0]
-	conTeam = Team.query.filter_by(id=debates.conTeamID).all()[0]
-	proargs = Argument.query.filter_by(debID=debates.id).filter_by(teamID=proTeam.id).all()
-	conargs = Argument.query.filter_by(debID=debates.id).filter_by(teamID=conTeam.id).all()
+	debates = models.Debate.query.limit(1).all()[0]
+	res = models.Resolution.query.filter_by(id=debates.resID).all()[0]
+	proTeam = models.Team.query.filter_by(id=debates.proTeamID).all()[0]
+	conTeam = models.Team.query.filter_by(id=debates.conTeamID).all()[0]
+	proargs = models.Argument.query.filter_by(debID=debates.id).filter_by(teamID=proTeam.id).all()
+	conargs = models.Argument.query.filter_by(debID=debates.id).filter_by(teamID=conTeam.id).all()
 	return render_template('debate.html', resolution = res, pro = proTeam, con = conTeam, proarg = proargs, conarg = conargs)
 
 
