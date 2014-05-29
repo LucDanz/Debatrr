82a05dcabe67f95448e69e45fca798f32438d1f3
Eric Danz <ericdanziger@gmail.com>
2014-05-28 22:33:06 -0400

    added secret key

--- debatr/__init__.py
+++ debatr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, session
 import datetime
 import bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -8,6 +8,7 @@ from flask import render_template
 
 app = Flask(__name__)
 app.config.from_object('debatr.config')
+app.secret_key = '\xeb\xca\x0bb\xdb\xa1\x8c\xf4@6Ar H%\xb00\xd8(\x81\xa4\xf8\xa5\xee'
 #['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/deb.db"
 db = SQLAlchemy(app)
 
